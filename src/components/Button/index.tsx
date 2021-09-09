072e3eb57addcccb8ba9bea4c8fd9e3d3c7e1ab3
fabio <fabio_santos_2001@outlook.com>
2021-09-09 20:32:05 -0300

    Button Component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler';
+import { Container, ButtonText } from './styles';
+
+interface ButtonProps extends RectButtonProperties {
+  children: string;
+}
+
+export const Button = ({ children, ...rest }: ButtonProps) => {
+  return (
+    <Container {...rest}>
+      <ButtonText>{children}</ButtonText>
+    </Container>
+  ); 
+};
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Image } from 'react-native';
+
+//@ts-expect-error
 import logoImg from '../../assets/logo.png';
+
+//components
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
 
 import { Container, Title } from './styles';
 
@@ -9,6 +15,9 @@ export const SignIn = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu Logon</Title>
+      <Input />
+      <Input />
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
